c5785b420401917627bfdbdf89ab66d89bd4ddbf
arnavpatel82 <arnav.patel82@yahoo.com>
2022-05-11 17:45:50 +0800

    Version 1 is running
    - handles all current exceptions
    - asks if user wants to play game
    - prompts guess input
    - if input is wrong it will continue running
    - once input is correct, will ask user if they want to play again

--- number-game.py
+++ number-game.py
@@ -1,10 +1,45 @@
 import random
 
 
+def is_valid(number):
+    while number.isdigit() == False:
+            print("The guess must be a whole number")
+            number = input(">")
+    while int(number) < 1 or int(number) > 10:
+        print("The guess must be between 1 and 10")
+        number = input(">")
+    return number
+
+
 current_high_score = ""
 
 print("What is your name?")
 name = input(">")
-print(f"Welcome to the number guessing game, {name}")
 
-number = random.randint(1, 10)
+while True:
+    number = random.randint(1, 10)
+    print(f"Would you like to play the number guessing game, {name}? Please only enter 'yes' or 'no'")
+    y_n = input(">").lower()
+
+    while y_n != "yes" and y_n != "no":
+        print("Please only enter 'yes' or 'no'")
+        y_n = input(">")
+
+    if y_n == "yes":
+        print("What number do you guess? The guess must be a whole number between 1 and 10")
+        guess = input(">")
+        guess = is_valid(guess)
+        
+        while int(guess) != number:
+            print(f"Wrong guess! Try again, {name}")
+            print("What number do you guess? The guess must be a number between 1 and 10")
+            guess = input(">")
+            guess = is_valid(guess)
+
+        print(f"You guessed it! the number was {str(number)}")
+
+    elif y_n == "no":
+        print(f"Bye, {name}!")
+        break
+    
+
